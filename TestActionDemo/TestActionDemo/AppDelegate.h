d361403a1e9f87c37d314b5729bf0aa525ee6b7d
刘松 <sidliu@kugou.net>
2020-07-02 20:57:37 +0800

    初始化仓库

---
+++ TestActionDemo/TestActionDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestActionDemo
+//
+//  Created by rayor on 2020/7/2.
+//  Copyright © 2020 rayor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestActionDemo/TestActionDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestActionDemo
+//
+//  Created by rayor on 2020/7/2.
+//  Copyright © 2020 rayor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
